fa174aa07122048ab32deafa1845a94d3c2af424
alencarmachado <alencar.machado@hotmail.com>
2016-01-14 15:32:29 -0200

    projeto Git

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -16,6 +16,7 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         System.out.println("teste git ...");
+        System.out.println("novo app");
     }
     
 }
